63691be3925c4feada786bc0da277ffeadb508d1
Hamidusman <abdulhamidusman218@gmail.com>
2024-05-17 15:23:43 +0100

    login automation setup

---
+++ login-automation.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import  ChromeDriverManager
+
+
+url = "https://practicetestautomation.com/"
+
+service = Service(executable_path="chromedriver.exe")
+
+driver = webdriver.Chrome(service=service)
+driver.get(url)
+
+
+time.sleep(10)
+driver.quit()
